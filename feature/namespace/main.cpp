79ecf0f09995adb9375e4ae53c80102bc63a7f29
ZiaWang <850933437@qq.com>
2020-10-26 16:26:22 +0800

    更新namespace内容

--- feature/namespace/main.cpp
+++ feature/namespace/main.cpp
@@ -1,31 +1,60 @@
 #include <iostream>
 using namespace std;
 
+/* namespace
+ * 用来解决什么问题？
+ *    * 命名冲突（c语言中，所有的全局标识符是共享了一个作用域，因此标识符之间会存在命名冲突）
+ *
+ * 相关细节：
+ *    1. 将全局作用域区分为不同的部分。
+ *    2. 不同命名空间中，相同的标识符不会发生冲入
+ *    3. 命名空间可以嵌套（比如namespace中可以定义其他namespace）
+ *    4. 全局命名空间实际上是默认的一个namespace，即默认命名空间。
+ *
+ * 使用：
+ *    使用整个命名空间：
+ *          using namespace std;
+ *
+ *    使用命名空间中指定的变量来覆盖当前默认空间的变量：
+ *          using using boost::shared_ptr;
+ *
+ *    使用默认命名空间中的变量：
+ *          ::var_name
+ *
+ *
+ *
+ */
+
+
+const char *a = "default namespace";
+
+
+
 // namespace只能在全局或者其他namespace中定义
 namespace n1{
-    int a = 1;
+    const char *a = "namespace n1";
 }
 
 
 namespace n2{
-    int a = 2;
+    const char *a = "namespace n2";
 }
 
 
 void std_namespace(){
-    using namespace n1;
-    std::cout << "n1::a = " << a << std::endl;
+    cout << "a =  " <<  a << endl;          // a = default namespace
+    cout << "::a =  " <<  ::a << endl;      // ::a = default namespace
 
-    using n2::a;
-    std::cout << "n2::a = " << a << std::endl;
-    std::cout << "n1::a = " << n1::a << std::endl;
+    cout << "n1::a = " << n1::a << endl;    // n1::a = namespace n1
+
+    cout << "n2::a = " << n2::a << endl;    // n2::a = namespace n2
+
+    using n1::a;
+    cout << "a =  " <<  a << endl;          // a = n1
+
+    cout << "::a =  " <<  ::a << endl;          // a = default namespace
 }
 
 int main() {
-/*  名称空间
-    int a = 3;
     std_namespace();
-    //局部命名空间中调用using namespace xxx 或者using xxx::var，只会在该局部环境中生效，并且会覆盖掉上层命名空间中的内容。
-    std::cout << "global::a = " << a << std::endl;
-*/
 }
